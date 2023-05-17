23f851ec56ff50bc1b8f9e4cd77f2b0ed52c269b
jackhsieh806 <133832052+jackhsieh806@users.noreply.github.com>
2023-05-17 21:41:07 +0800

    Update comparefunc.c

--- comparefunc.c
+++ comparefunc.c
@@ -1,12 +1,14 @@
-    struct cmp {
+//small to big (top is smallest)   
+struct cmp {
         bool operator() (pair<int, int> &a, pair<int, int> &b) {
             return a.first + a.second > b.first + b.second;
         }
     };
+
     static bool compare2(pair<int,int> &n1,pair<int,int> &n2) {
         return n1.first+n1.second < n2.first+n2.second;
     }    
-
+//small to big (vector[0] is smallest)
     static bool compare(vector<int>& n1,vector<int>& n2) {
         return n1[0]+n1[1] < n2[0]+n2[1];
     }
