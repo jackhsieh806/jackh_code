0189c5b4ac1f50d1a5ce2984cb7d8c490b958381
jackhsieh806 <133832052+jackhsieh806@users.noreply.github.com>
2023-05-18 09:08:23 +0800

    Update sort_example.c

--- sort_example.c
+++ sort_example.c
@@ -1,2 +1,27 @@
+/*
 sort(begin, end, less<Type>()); // 6 5 4 3 2 1
 sort(begin, end, greater<Type>()) //1 2 3 4 5 6  //default
+sort(v.begin(), v.end(), [](int a, int b){
+        return a < b; // 升序排列
+    });
+
+priority_queue<int, vector<int>, greater<int> > q; //top = smallest == v[0] //not default
+*/
+void printQueue(T& q)
+{
+    while (!q.empty())
+    {
+        cout << q.top() << endl;
+        q.pop();
+    }
+}
+
+void SamplePriorityQueue()
+{
+    priority_queue<int, vector<int>, greater<int> > q;
+
+    for(int n : {1,8,5,6,3,4,0,9,7,2})
+        q.push(n);
+
+    printQueue(q);
+}
